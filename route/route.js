7f883f6d0b8fc8a874695363bcb9c37078a60011
Tanmay Mundada <soorajmundada@gmail.com>
2023-05-15 21:37:19 +0530

    bro

--- route/route.js
+++ route/route.js
@@ -488,7 +488,7 @@ router.get("/logout", (req, res) => {
 });
 
 router.get("*", (req, res) => {
-    res.send("<h1>Heyy there!</h1>")
+    res.send("<h1>Heyy there! Finally</h1>")
 });
 
 module.exports = router;
